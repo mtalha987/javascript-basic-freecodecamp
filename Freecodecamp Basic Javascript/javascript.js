084a0b45964793476c3b9c571db00832c17e78ad
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2022-01-06 00:08:55 +0500

    free code camp first 15

---
+++ Freecodecamp Basic Javascript/javascript.js
@@ -0,0 +1,97 @@
+//--------------------How to comment in Java Script----------------------- //
+
+
+
+//This is a Comment
+
+/* this is multiple
+ line comment
+*/
+
+// -----------The End ----------------//
+
+
+//---------How to declare variable and intiliaze variable in javascript ---------//
+
+//  var myName ;
+
+//  var a;
+// /a=7;
+
+
+//  var a;
+//  a = 7;
+// var b;
+//  b=a;
+
+// var a=9;
+
+// var myFirstName= "talha";
+// var myLastName= "rehman";
+
+
+// var a;
+// var b;
+// var c;
+// a=5;
+// b=10;
+// c="I am a";
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+
+
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+
+
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
+
+
+//-----------------------------------THE END ------------------------------------//
+
+
+//-----------------------Add Two Numbers with JavaScript-------------------------//
+
+
+// const sum = 10 + 10;
+
+// // ------------------------Subtract One Number from Another with JavaScript------------//
+
+// const difference = 45 - 33;
+
+// // -------------------Multiply---------------//
+// const product = 8 * 10;
+
+
+
+// // --------------------Divide---------------//
+
+// const quotient = 66 /33;
+
+
+// //--------------Increment----------------//
+
+// let myVar = 87;
+
+// // Only change code below this line
+// myVar++;
+
+
+// // -----------------Decrement-----------//
